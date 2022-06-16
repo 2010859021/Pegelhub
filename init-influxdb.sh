b628c0860b1aea27c83cbf2d38e23029a92d970f
Marc Urschick <marc@urschick.at>
2022-06-17 00:10:06 +0200

    added some influx stuff and more configuration properties

--- init-influxdb.sh
+++ init-influxdb.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e
-influx setup --username $ADMIN_USER --password $ADMIN_PASSWORD --org $STORE_USER --bucket $BUCKET_INTERNAL
 influx user create --name $STORE_USER --password $STORE_PASSWORD
 
 influx bucket create --name $BUCKET_TELEMETRY
@@ -14,7 +13,9 @@ echo "data:" > /etc/influxdb2/storeapp
 echo "  org: $STORE_USER" >> /etc/influxdb2/storeapp
 echo "  token: $TOKEN" >> /etc/influxdb2/storeapp
 echo "  bucket: $BUCKET_DATA" >> /etc/influxdb2/storeapp
+echo "  url: none" >> /etc/influxdb2/storeapp
 echo "telemetry:" >> /etc/influxdb2/storeapp
 echo "  org: $STORE_USER" >> /etc/influxdb2/storeapp
 echo "  token: $TOKEN" >> /etc/influxdb2/storeapp
 echo "  bucket: $BUCKET_TELEMETRY" >> /etc/influxdb2/storeapp
+echo "  url: none" >> /etc/influxdb2/storeapp
