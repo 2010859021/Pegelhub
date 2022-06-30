32a86998ae99113355cf10dd3bf5635d7a141d99
Marc Urschick <marc@urschick.at>
2022-06-30 21:50:01 +0200

    looks like the final build

--- init-influxdb.sh
+++ init-influxdb.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 set -e
-influx user create --name $STORE_USER --password $STORE_PASSWORD
+influx user create --name "$STORE_USER" --password "$STORE_PASSWORD"
 
-influx bucket create --name $BUCKET_TELEMETRY
-influx bucket create --name $BUCKET_DATA
+influx bucket create --name "$BUCKET_TELEMETRY"
+influx bucket create --name "$BUCKET_DATA"
 
-TOKEN=$(influx auth create --user $STORE_USER --read-buckets --write-buckets | tail -n -1 | cut -f 4)
-
-touch /etc/influxdb2/storeapp
-echo "data:" > /etc/influxdb2/storeapp
-echo "  org: $STORE_USER" >> /etc/influxdb2/storeapp
-echo "  token: $TOKEN" >> /etc/influxdb2/storeapp
-echo "  bucket: $BUCKET_DATA" >> /etc/influxdb2/storeapp
-echo "  url: none" >> /etc/influxdb2/storeapp
-echo "telemetry:" >> /etc/influxdb2/storeapp
-echo "  org: $STORE_USER" >> /etc/influxdb2/storeapp
-echo "  token: $TOKEN" >> /etc/influxdb2/storeapp
-echo "  bucket: $BUCKET_TELEMETRY" >> /etc/influxdb2/storeapp
-echo "  url: none" >> /etc/influxdb2/storeapp
+TOKEN=$(influx auth create --user "$STORE_USER" --read-buckets --write-buckets | tail -n -1 | cut -f 4)
+OUT_FILE="/etc/influxdb2/store/storeapp.yaml"
+touch $OUT_FILE
+echo "data:" > $OUT_FILE
+echo "  org: $STORE_USER" >> $OUT_FILE
+echo "  token: $TOKEN" >> $OUT_FILE
+echo "  bucket: $BUCKET_DATA" >> $OUT_FILE
+echo "  url: none" >> $OUT_FILE
+echo "telemetry:" >> $OUT_FILE
+echo "  org: $STORE_USER" >> $OUT_FILE
+echo "  token: $TOKEN" >> $OUT_FILE
+echo "  bucket: $BUCKET_TELEMETRY" >> $OUT_FILE
+echo "  url: none" >> $OUT_FILE
